bf7d70f92681fe439daf6d80ba02cfcf83275d85
MatheusHoffmann <matheus_mauricio12@hotmail.com>
2015-04-28 22:38:56 -0300

    Create UsuarioDAOImpl.java

---
+++ persistence/src/main/java/br/com/alvoradamaringa/persistence/UsuarioDAOImpl.java
@@ -0,0 +1,7 @@
+package br.com.alvoradamaringa.persistence;
+
+import br.com.alvoradamaringa.domain;
+
+public class UsuarioDAOImpl extends GenericDAOImpl<Long, Usuario> implements UsuarioDAO {
+
+}
