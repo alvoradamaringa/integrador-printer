db254cc8ffeb4954d0845773436195662f248128
Diego de Oliveira Alves Pereira <dpereira@uolinc.com>
2015-05-04 19:53:10 -0300

    update

--- domain/src/main/java/br/com/alvoradamaringa/domain/DefesaComentario.java
+++ domain/src/main/java/br/com/alvoradamaringa/domain/DefesaComentario.java
@@ -7,10 +7,6 @@ public class DefesaComentario extends DefesaComentarioDAOImpl {
     private ProfessorCurso professorComentario;
     private String comentario;
 
-    public void comentarDefesa() {
-
-    }
-
     public DefesaAluno getDefesaComentario() {
         return defesaComentario;
     }
