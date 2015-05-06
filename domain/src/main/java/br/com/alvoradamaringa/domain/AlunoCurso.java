4f1f0ee4639b9e22a674a5700ca60f32da19de8d
Pedro Paulo Boveto <pedroboveto@gmail.com>
2015-05-06 20:00:48 -0300

    update AlunoCurso

--- domain/src/main/java/br/com/alvoradamaringa/domain/AlunoCurso.java
+++ domain/src/main/java/br/com/alvoradamaringa/domain/AlunoCurso.java
@@ -5,5 +5,25 @@ public class AlunoCurso {
     private BigInteger idAlunoCurso;
     private Aluno idAluno;
     private Curso idCurso;
+    
+     public void setId(BigIntege idAlunoCurso){
+          this.idAlunoCursoo=idAlunoCurso;
+    }
+    public Integer getId(BigIntege idAluno){
+          return this.idAluno;
+    }
+    public void setId(Curso idcurso){
+          this.idcurso=idcurso;
+     }
+    public Integer getId(Curso idcurso){
+          return this.idcurso;
+    }
+    public void setId(Aluno idAluno){
+          this.idAluno=idAluno;
+    }
+    public Integer getId(Aluno idAluno){
+          return this.idAluno;
+    }
+    
 
 }
