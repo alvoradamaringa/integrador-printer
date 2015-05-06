e60ac56ed27d7cc5bfabbb34ee11faac83122509
Brunoaggarcia <brunoaggarcia@hotmail.com>
2015-05-06 00:27:07 -0300

    Update ProfessorCurso.java

--- domain/src/main/java/br/com/alvoradamaringa/domain/ProfessorCurso.java
+++ domain/src/main/java/br/com/alvoradamaringa/domain/ProfessorCurso.java
@@ -4,4 +4,29 @@ public class ProfessorCurso {
 	private Integer idProfessorCurso;
 	private Integer idCurso;
 	private Integer idProfessor
+		
+	public Integer getIdProfessorCurso(){
+		return idProfessorCurso;
+	}
+	
+	public void setIdProfessorCurso(Integer idProfessorCurso){
+		this.idProfessorCurso = idProfessorCurso;
+	}
+	
+	public Integer getIdCurso(){
+		return idCurso;
+	}
+	
+	public void setIdCurso(Integer idCurso){
+		this.idCurso = idCurso;
+	}
+	
+	public Integer getIdProfessor(){
+		return idProfessor;
+	}
+	
+	public void setIdProfessor(Integer idProfessor){
+		this.idProfessor = idProfessor;
+	}
+	
 }
