1a44cf57a272992964ae7f38a1a25adedb8d1477
Diego de Oliveira Alves Pereira <diego_oliveira_alves@hotmail.com>
2015-05-08 15:37:32 -0300

    rename DefesaComentario method
    método será de consulta

--- persistence/src/main/java/br/com/alvoradamaringa/persistence/DefesaComentarioDAOImpl.java
+++ persistence/src/main/java/br/com/alvoradamaringa/persistence/DefesaComentarioDAOImpl.java
@@ -5,7 +5,7 @@ import br.com.alvoradamaringa.domain;
 public class DefesaComentarioDAOImpl extends GenericDAOImpl<Long, DefesaComentario> implements DefesaComentarioDAO {
 
     @Override
-    public void adicionarComentario() {
+    public void consultar() {
 
     }
 
