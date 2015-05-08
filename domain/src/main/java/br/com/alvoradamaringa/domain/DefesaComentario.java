cc0e429a781091077022f6e23f9245ddb9a6c9fd
Diego de Oliveira Alves Pereira <diego_oliveira_alves@hotmail.com>
2015-05-08 14:50:14 -0300

    correção - DefesaComentario

--- domain/src/main/java/br/com/alvoradamaringa/domain/DefesaComentario.java
+++ domain/src/main/java/br/com/alvoradamaringa/domain/DefesaComentario.java
@@ -2,33 +2,8 @@ package br.com.alvoradamaringa.domain;
 
 public class DefesaComentario extends DefesaComentarioDAOImpl {
 
-    private DefesaAluno defesaComentario;
-    private ProfessorCurso professorComentario;
+    private Long idDefesaComentario;
     private String comentario;
-
-    public DefesaAluno getDefesaComentario() {
-        return defesaComentario;
-    }
-
-    public void setDefesaComentario(DefesaAluno defesaComentario) {
-        this.defesaComentario = defesaComentario;
-    }
-
-    public ProfessorCurso getProfessorComentario() {
-        return professorComentario;
-    }
-
-    public void setProfessorComentario(ProfessorCurso professorComentario) {
-        this.professorComentario = professorComentario;
-    }
-
-    public String getComentario() {
-        return comentario;
-    }
-
-    public void setComentario(String comentario) {
-        this.comentario = comentario;
-    }
 
     public void adicionarComentario(String comentario) {
         this.comentario = comentario;
