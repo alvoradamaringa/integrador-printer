bda09bbcf665887fde8abef53cae874ca4d4e0ab
Valter Machado <valtermac@gmail.com>
2015-05-10 20:22:24 -0300

    Update Aluno.java
    Adicionado os Get e Set.

--- domain/src/main/java/br/com/alvoradamaringa/domain/Aluno.java
+++ domain/src/main/java/br/com/alvoradamaringa/domain/Aluno.java
@@ -2,6 +2,26 @@ package br.com.alvoradamaringa.domain;
 
 public class Aluno{
 	private long idAluno;
-	private Pessoa pessoa;
-	private String ra;
+	private pessoa idPessoa;
+	private String Ra;
 }
+	private long getidAluno(){
+		return this.idAluno;
+	}
+		private void setIdAluno(long idAluno){
+			this.idAlunoCurso = idAluno;
+		}
+		
+			private pessoa getidPessoa(){
+				return this.idPessoa;
+			}
+				private void setidPessoa(pessoa idPessoa){
+					this.idPessoa = idPessoa;
+				}
+					
+					private String getRa(){
+						return this.ra;
+						}
+							private void setStringRa(ra String Ra){
+								this.String Ra = Ra;
+							}
