0cc51e478a6c28a8bcaec205b08d5fe61aed9131
Brunoaggarcia <brunoaggarcia@hotmail.com>
2015-05-11 18:47:20 -0300

    Update ProfessorCursoDAOImpl.java

--- persistence/src/main/java/br/com/alvoradamaringa/persistence/ProfessorCursoDAOImpl.java
+++ persistence/src/main/java/br/com/alvoradamaringa/persistence/ProfessorCursoDAOImpl.java
@@ -3,5 +3,9 @@ package br.com.alvoradamaringa.persistence;
 import br.com.alvoradamaringa.domain;
 
 public class ProfessorCursoDAOImpl extends GenericDAOImpl<Long, ProfessorCurso> implements ProfessorCursoDAO {
+  
+   private void consultaProfessorCurso(Long idProfessorCurso){
+     
+   }
 
 }
