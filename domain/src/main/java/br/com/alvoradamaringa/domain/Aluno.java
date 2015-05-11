9b71da80b6de232ef3294783b0274a204df30afb
Valter Machado <valtermac@gmail.com>
2015-05-11 14:08:52 -0300

    Update Aluno.java

--- domain/src/main/java/br/com/alvoradamaringa/domain/Aluno.java
+++ domain/src/main/java/br/com/alvoradamaringa/domain/Aluno.java
@@ -4,24 +4,25 @@ public class Aluno{
 	private long idAluno;
 	private pessoa idPessoa;
 	private String Ra;
-}
+	
 	private long getidAluno(){
 		return this.idAluno;
 	}
-		private void setIdAluno(long idAluno){
-			this.idAlunoCurso = idAluno;
-		}
+	private void setIdAluno(long idAluno){
+		this.idAlunoCurso = idAluno;
+	}
 		
-			private pessoa getidPessoa(){
-				return this.idPessoa;
-			}
-				private void setidPessoa(pessoa idPessoa){
-					this.idPessoa = idPessoa;
-				}
+	private pessoa getidPessoa(){
+		return this.idPessoa;
+	}
+	private void setidPessoa(pessoa idPessoa){
+		this.idPessoa = idPessoa;
+	}
 					
-					private String getRa(){
-						return this.ra;
-						}
-							private void setStringRa(ra String Ra){
-								this.String Ra = Ra;
-							}
+	private String getRa(){
+		return this.ra;
+	}
+	private void setStringRa(ra String Ra){
+		this.String Ra = Ra;
+	}
+}
