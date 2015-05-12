3d26044659d875f0f9c764830c9d29841009cd41
Diego de Oliveira Alves Pereira <diego_oliveira_alves@hotmail.com>
2015-05-12 04:19:35 -0300

    (correção) Curso
    - Usar "Long" ao invés de "long"
    - Não tem comportamentos e getters and setters
    
    (plus) DAOImpl

--- domain/src/main/java/br/com/alvoradamaringa/domain/Curso.java
+++ domain/src/main/java/br/com/alvoradamaringa/domain/Curso.java
@@ -1,20 +1,8 @@
 package br.com.alvoradamaringa.domain;
 
-public class Curso {
-     private long idCurso;
-     private String descricao;
-     
-     public void setId(Integer idcurso){
-          this.idcurso=idcurso;
-     }
-     public void setDescricao(String descricao){
-          this.descricao=descricao;
-     }
-     public Integer getId(Integer idcurso){
-          return this.idcurso;
-     }
-     public String getdescricao(String descricao){
-          return this.descricao;
-     }
-     
+public class Curso extends CursoDAOImpl {
+
+    private Long idCurso;
+    private String descricao;
+
 }
---
+++ persistence/src/main/java/br/com/alvoradamaringa/persistence/CursoDAOImpl.java
@@ -0,0 +1,7 @@
+package br.com.alvoradamaringa.persistence;
+
+import br.com.alvoradamaringa.domain;
+
+public class CursoDAOImpl extends GenericDAO<Long, Curso> implements CursoDAO {
+
+}
