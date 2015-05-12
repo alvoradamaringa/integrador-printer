35ec2efa1b6fb8f8e5137c33b8c4082e45835c5e
Diego de Oliveira Alves Pereira <diego_oliveira_alves@hotmail.com>
2015-05-12 04:58:00 -0300

    (correção) PessoaDAO & DAOImpl
    apenas adicionado pacote e import

--- persistence/src/main/java/br/com/alvoradamaringa/persistence/PessoaDAO.java
+++ persistence/src/main/java/br/com/alvoradamaringa/persistence/PessoaDAO.java
@@ -1,2 +1,7 @@
-public interface PessoaDAO extends GenericDAO<Long, Pessoa>{
+package br.com.alvoradamaringa.persistence;
+
+import br.com.alvoradamaringa.domain;
+
+public interface PessoaDAO extends GenericDAO<Long, Pessoa> {
+
 }
--- persistence/src/main/java/br/com/alvoradamaringa/persistence/PessoaDAOImpl.java
+++ persistence/src/main/java/br/com/alvoradamaringa/persistence/PessoaDAOImpl.java
@@ -1,7 +1,11 @@
+package br.com.alvoradamaringa.persistence;
+
+import br.com.alvoradamaringa.domain;
+
 public class PessoaDAOImpl extends GenericDAOImpl<Long, Pessoa> implements PessoaDAO {
 
-  private void consultaPessoa(Long idPessoa){
-    
-  }
+    private void consultaPessoa(Long idPessoa) {
+
+    }
 
 }
