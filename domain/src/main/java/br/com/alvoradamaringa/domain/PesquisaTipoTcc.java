18b28562b2c6aeb3b821c5ca94a4a90959cfea3e
Marcelo Beani da Costa Junior <marcelo666junior666@gmail.com>
2015-05-12 09:32:53 -0300

    Update PesquisaTipoTcc.java

--- domain/src/main/java/br/com/alvoradamaringa/domain/PesquisaTipoTcc.java
+++ domain/src/main/java/br/com/alvoradamaringa/domain/PesquisaTipoTcc.java
@@ -4,21 +4,5 @@ public class PesquisaTipoTcc {
     private PesquisaTipoTcc pesquisaTipoTcc;
     private String descricao;
 	
-	
-	public void setpesquidatipocc(Long pesquidatipocc){
-		this.pesquidatipocc = pesquidatipocc;
-	}
-	
-	public Integer getpesquidatipocc(){
-		return this.pesquidatipocc;
-	}
-	
-	public void setdescricao(String descricao){
-		this.descricao = descricao;
-	}
-	
-	public String getdescricao(){
-		return this.descricao;
-	
-	}
+}
 	
