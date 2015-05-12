06fcfaf68b0ec496123232443aef2ecd5bab98ce
kazumy <contato@ogaweb.com>
2015-05-11 21:27:43 -0300

    Update BancaProfessorDAOImpl.java

--- persistence/src/main/java/br/com/alvoradamaringa/persistence/BancaProfessorDAOImpl.java
+++ persistence/src/main/java/br/com/alvoradamaringa/persistence/BancaProfessorDAOImpl.java
@@ -3,5 +3,10 @@ package br.com.alvoradamaringa.persistence;
 import br.com.alvoradamaringa.domain;
 
 public class BancaProfessorDAOImpl extends GenericDAOImpl<Long, BancaProfessor> implements BancaProfessorDAO {
+  
+    @Override
+    public void consultarBancaProfessor() {
+
+    }
 
 }
