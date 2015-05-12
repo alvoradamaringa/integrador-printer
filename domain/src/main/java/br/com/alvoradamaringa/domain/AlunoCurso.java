8326d6ae9abe19cf996236d84f2070033477ee00
Diego de Oliveira Alves Pereira <diego_oliveira_alves@hotmail.com>
2015-05-12 04:06:19 -0300

    (correção) AlunoCurso
    - Usar "Long" ao invés de "BigInteger"
    - Usar "aluno" ao invés de "idAluno"
    - Usar "curso" ao invés de "idCurso"
    - Não tem comportamentos e getters and setters

--- domain/src/main/java/br/com/alvoradamaringa/domain/AlunoCurso.java
+++ domain/src/main/java/br/com/alvoradamaringa/domain/AlunoCurso.java
@@ -1,29 +1,9 @@
+package br.com.alvoradamaringa.domain;
 
-import java.math.BigInteger;
+public class AlunoCurso extends AlunoCursoDAOImpl {
 
-public class AlunoCurso {
-    private BigInteger idAlunoCurso;
-    private Aluno idAluno;
-    private Curso idCurso;
-    
-     public void setId(BigIntege idAlunoCurso){
-          this.idAlunoCursoo=idAlunoCurso;
-    }
-    public Integer getId(BigIntege idAluno){
-          return this.idAluno;
-    }
-    public void setId(Curso idcurso){
-          this.idcurso=idcurso;
-     }
-    public Integer getId(Curso idcurso){
-          return this.idcurso;
-    }
-    public void setId(Aluno idAluno){
-          this.idAluno=idAluno;
-    }
-    public Integer getId(Aluno idAluno){
-          return this.idAluno;
-    }
-    
+    private Long idAlunoCurso;
+    private Aluno aluno;
+    private Curso curso;
 
 }
