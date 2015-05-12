d74582bd363df6ec88c3c37e7b7be14c53aa2f56
MatheusHoffmann <matheus_mauricio12@hotmail.com>
2015-05-11 21:46:58 -0300

    Update UsuarioDAO.java

--- persistence/src/main/java/br/com/alvoradamaringa/persistence/UsuarioDAO.java
+++ persistence/src/main/java/br/com/alvoradamaringa/persistence/UsuarioDAO.java
@@ -4,4 +4,8 @@ import br.com.alvoradamaringa.domain;
 
 public interface UsuarioDAO extends GenericDAO<Long, Usuario>{
     
+    public void consultar(String login, String nome){
+    
+    }
+    
 }
