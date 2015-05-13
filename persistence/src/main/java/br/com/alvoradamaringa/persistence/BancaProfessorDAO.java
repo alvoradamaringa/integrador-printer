0656857d224e275544341d4b24abc9dd92b635f0
kazumy <contato@ogaweb.com>
2015-05-13 13:26:58 -0300

    Update BancaProfessorDAO.java

--- persistence/src/main/java/br/com/alvoradamaringa/persistence/BancaProfessorDAO.java
+++ persistence/src/main/java/br/com/alvoradamaringa/persistence/BancaProfessorDAO.java
@@ -4,6 +4,8 @@ import br.com.alvoradamaringa.domain;
 
 public interface BancaProfessorDAO extends GenericDAO<Long, BancaProfessor>{
   
-   public List<BancaProfessor> consultar(String nomeAluno, String nomeProfessor);
+   public List<BancaProfessor> consultar(String nomeAluno, String nomeProfessor){
+       return null;
+   }
 
 }
