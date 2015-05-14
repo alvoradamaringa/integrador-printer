a6fa82554890be591b975801a6799d9e56834337
Altieres de Matos <altitdb@gmail.com>
2015-05-14 09:30:17 -0300

    Mapeamento Classe Aluno
    Exemplo de como deve ser feito o mapeamento Objeto relacional utilizando
    JPA.

--- domain/src/main/java/br/com/alvoradamaringa/domain/Aluno.java
+++ domain/src/main/java/br/com/alvoradamaringa/domain/Aluno.java
@@ -1,10 +1,24 @@
 package br.com.alvoradamaringa.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Aluno {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ID_ALUNO")
 	private Long idAluno;
+	@ManyToOne
+	@JoinColumn(name = "ID_PESSOA")
 	private Pessoa pessoa;
-	private String Ra;
+	private String ra;
 	
 	public void adicionarPessoa(Pessoa pessoa) {
 		this.pessoa = pessoa;
@@ -23,11 +37,11 @@ public class Aluno {
 	}
 
 	public String getRa() {
-		return Ra;
+		return ra;
 	}
 
 	public void setRa(String ra) {
-		Ra = ra;
+		this.ra = ra;
 	}
 
 }
