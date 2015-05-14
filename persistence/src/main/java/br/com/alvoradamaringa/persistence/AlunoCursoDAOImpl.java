15dd4e28f1de212276e5a33ec418ad838f3fbdec
eletroship <eletroship@hotmail.com>
2015-05-13 21:58:05 -0300

    Update AlunoCursoDAOImpl.java

--- persistence/src/main/java/br/com/alvoradamaringa/persistence/AlunoCursoDAOImpl.java
+++ persistence/src/main/java/br/com/alvoradamaringa/persistence/AlunoCursoDAOImpl.java
@@ -3,5 +3,7 @@ package br.com.alvoradamaringa.persistence;
 import br.com.alvoradamaringa.domain;
 
 public class AlunoCursoDAOImpl extends GenericDAOImpl<Long, AlunoCurso> implements AlunoCursoDAO {
+  
+  public list<AlunoCurso> Consultar(String aluno,String curso,long idAlunoCurso);
 
 }
