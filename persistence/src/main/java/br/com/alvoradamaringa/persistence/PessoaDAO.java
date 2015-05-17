ca0738cf6c44c9c600c8be1a2222ebc02f892545
Fernando Wesley <fernando.wesleypf@gmail.com>
2015-05-17 12:57:20 -0300

    Update PessoaDAO.java

--- persistence/src/main/java/br/com/alvoradamaringa/persistence/PessoaDAO.java
+++ persistence/src/main/java/br/com/alvoradamaringa/persistence/PessoaDAO.java
@@ -3,5 +3,5 @@ package br.com.alvoradamaringa.persistence;
 import br.com.alvoradamaringa.domain;
 
 public interface PessoaDAO extends GenericDAO<Long, Pessoa> {
-	
+	public List<Pessoa> consultar(String nome, String cpf);	
 }
