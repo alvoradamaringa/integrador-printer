2bcf01a6828d5d5cca010d685da2c562e1c37626
Fernando Wesley <fernando.wesleypf@gmail.com>
2015-05-17 12:59:23 -0300

    Update PessoaDAOImpl.java

--- persistence/src/main/java/br/com/alvoradamaringa/persistence/PessoaDAOImpl.java
+++ persistence/src/main/java/br/com/alvoradamaringa/persistence/PessoaDAOImpl.java
@@ -4,8 +4,8 @@ import br.com.alvoradamaringa.domain;
 
 public class PessoaDAOImpl extends GenericDAOImpl<Long, Pessoa> implements PessoaDAO {
 
-	private void consultaPessoa(Long idPessoa) {
-
-	}
+	public List<Pessoa> consultar(String nome, String cpf){
+    		return null;
+  	}
 
 }
