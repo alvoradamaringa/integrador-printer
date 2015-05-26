0a2efaac4559ef6b73c9a5afb1e149312fd63057
Pedro Henrique Fernandes <pedrohenrique_fernandes@hotmail.com>
2015-05-26 00:14:08 -0300

    Update TipoProfessor.java

--- domain/src/main/java/br/com/alvoradamaringa/domain/TipoProfessor.java
+++ domain/src/main/java/br/com/alvoradamaringa/domain/TipoProfessor.java
@@ -1,6 +1,45 @@
 package br.com.alvoradamaringa.domain;
 
-public enum TipoProfessor {
+import java.io.Serializable;
+import javax.swing.text.html.parser.Entity;
 
-	ORIENTADOR, CONVIDADO;
+/**
+ *
+ * @author Pedro
+ */
+@Entity
+public class TipoProfessor implements Serializable {
+
+	private static final long serialVersionUID = 23L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID_TipoProfessor")
+	private Long idTipoProfessor;
+	private String Orientador;
+	private String Convidado;
+    private Object GenerationType;
+
+	public Long getTipoProfessor() {
+		return idTipoProfessor;
+	}
+
+	public void setIdTipoProfessor(Long idTipoProfessor) {
+		this.idTipoProfessor = idTipoProfessor;
+	}
+
+	public void setOrientador(String Orientador) {
+		this.Orientador = Orientador;
+	}
+
+	public String getOrientador() {
+		return this.Orientador;
+	}
+
+	public void setConvidado(String Convidado) {
+		this.Convidado = Convidado;
+	}
+
+	public String getConvidado() {
+		return this.Convidado;
+	}
 }
