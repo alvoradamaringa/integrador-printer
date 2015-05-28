ce2945dd22a822f4a57f91c143cda3874a6f81a3
Altieres de Matos <altitdb@gmail.com>
2015-05-27 22:52:55 -0300

    Bussiness

--- persistence/src/main/java/br/com/alvoradamaringa/persistence/UsuarioDAO.java
+++ persistence/src/main/java/br/com/alvoradamaringa/persistence/UsuarioDAO.java
@@ -1,11 +1,9 @@
 package br.com.alvoradamaringa.persistence;
 
-import java.util.List;
-
 import br.com.alvoradamaringa.domain.Usuario;
 
 public interface UsuarioDAO extends GenericDAO<Long, Usuario> {
 
-	public List<Usuario> consultar(String login, String nome);
+	public Usuario consultar(String login, String nome);
 	
 }
