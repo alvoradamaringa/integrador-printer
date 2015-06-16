d5060304cfa79f48039dc3d259c0cbd25b699c45
Altieres de Matos <altitdb@gmail.com>
2015-06-16 15:04:12 -0300

    Correção Erros de Compilação

--- business/src/main/java/br/com/alvoradamaringa/service/spec/UsuarioService.java
+++ business/src/main/java/br/com/alvoradamaringa/service/spec/UsuarioService.java
@@ -1,13 +1,13 @@
-import br.com.alvoradamaringa.domain.Usuario;
+package br.com.alvoradamaringa.service.spec;
 
+import br.com.alvoradamaringa.domain.Usuario;
 
 public interface UsuarioService {
 
 	public void consultarUsuario(String nome, String NivelUsuario);
-	
-	
+
 	public void editarUsuario(Usuario usuario);
-	
-	
+
 	public void excluirUsuario(Usuario usuario);
-	
+
+}
