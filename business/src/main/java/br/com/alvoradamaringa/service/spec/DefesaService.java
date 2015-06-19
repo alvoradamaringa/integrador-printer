41db79ea2483e1f598f3b0ef8b7c6c6c1d6c05f6
Altieres de Matos <altitdb@gmail.com>
2015-06-18 21:32:33 -0300

    Specification

--- business/src/main/java/br/com/alvoradamaringa/service/spec/DefesaService.java
+++ business/src/main/java/br/com/alvoradamaringa/service/spec/DefesaService.java
@@ -8,15 +8,18 @@ import javax.ejb.Local;
 import br.com.alvoradamaringa.domain.BancaProfessor;
 import br.com.alvoradamaringa.domain.DefesaAluno;
 import br.com.alvoradamaringa.domain.DefesaComentario;
+import br.com.alvoradamaringa.service.exceptions.AlunoNaoInformadoException;
+import br.com.alvoradamaringa.service.exceptions.BancaException;
+import br.com.alvoradamaringa.service.exceptions.IntegridadeException;
 
 @Local
 public interface DefesaService {
 
 	List<DefesaAluno> consultarDefesa(Date data, String tema, String nomeAluno);
 
-	void salvarDefesa(List<BancaProfessor> bancaProfessores, DefesaAluno DefesaAluno);
+	void salvarDefesa(List<BancaProfessor> bancaProfessores, DefesaAluno DefesaAluno) throws AlunoNaoInformadoException, BancaException;
 
-    void excluirDefesa(DefesaAluno DefesaAluno);
+    void excluirDefesa(DefesaAluno DefesaAluno) throws IntegridadeException;
 
     void adicionarComentario(DefesaComentario DefesaComentario);
     
