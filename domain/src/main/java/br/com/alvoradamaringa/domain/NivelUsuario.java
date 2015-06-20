6d2e88a12904f95878c33ead3d9761ce8ab28643
Altieres de Matos <altitdb@gmail.com>
2015-06-19 23:39:17 -0300

    Ajuste NivelUsuario

--- domain/src/main/java/br/com/alvoradamaringa/domain/NivelUsuario.java
+++ domain/src/main/java/br/com/alvoradamaringa/domain/NivelUsuario.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -14,7 +15,7 @@ public class NivelUsuario  implements Serializable {
 	
 	private static final long serialVersionUID = 23L;
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "ID_NIVEL_USUARIO")
 	private Long idNivelUsuario;
 	private String descricao;
