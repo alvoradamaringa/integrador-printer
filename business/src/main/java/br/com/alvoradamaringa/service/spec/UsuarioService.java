1415532576d4f385c80e20935b5dd0881a78ec5e
Altieres de Matos <altitdb@gmail.com>
2015-06-24 23:55:01 -0300

    Revert "Update UsuarioService.java"

--- business/src/main/java/br/com/alvoradamaringa/service/spec/UsuarioService.java
+++ business/src/main/java/br/com/alvoradamaringa/service/spec/UsuarioService.java
@@ -1,39 +1,20 @@
-package br.com.alvoradamaringa.service.impl;
+package br.com.alvoradamaringa.service.spec;
 
 import java.util.List;
 
-import javax.ejb.EJB;
-import javax.ejb.Stateless;
+import javax.ejb.Local;
 
 import br.com.alvoradamaringa.domain.NivelUsuario;
 import br.com.alvoradamaringa.domain.Usuario;
-import br.com.alvoradamaringa.persistence.UsuarioDAO;
 import br.com.alvoradamaringa.service.exceptions.IntegridadeException;
-import br.com.alvoradamaringa.service.spec.UsuarioService;
 
-@Stateless
-public class UsuarioServiceBean implements UsuarioService {
+@Local
+public interface UsuarioService {
 
-	@EJB 
-	UsuarioDAO usuarioDAO;
-	
-	@Override
-	public List<Usuario> consultarUsuario(String nome, NivelUsuario nivelUsuario) {
-		return usuarioDAO.consultarUsuario(nome, nivelUsuario);
-	}
+	public List<Usuario> consultarUsuario(String nome, NivelUsuario nivelUsuario);
 
-	@Override
-	public void salvarUsuario(Usuario usuario) {
-		usuarioDAO.salvar(usuario);
-	}
+	public void salvarUsuario(Usuario usuario);
 
-	@Override
-	public void excluirUsuario(Usuario usuario) throws IntegridadeException {
-		try {
-			usuarioDAO.deletar(usuario);
-		}catch(Exception exception){
-			throw new IntegridadeException();
-		}
-	}
+	public void excluirUsuario(Usuario usuario) throws IntegridadeException;
 
 }
