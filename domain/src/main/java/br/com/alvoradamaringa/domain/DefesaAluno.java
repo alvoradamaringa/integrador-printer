da1988a36691237edfeea5a27c8beffabbc29cce
Altieres de Matos <altitdb@gmail.com>
2015-07-01 19:44:56 -0300

    Correção Merge

--- domain/src/main/java/br/com/alvoradamaringa/domain/DefesaAluno.java
+++ domain/src/main/java/br/com/alvoradamaringa/domain/DefesaAluno.java
@@ -41,7 +41,7 @@ public class DefesaAluno implements Serializable {
 	private PesquisaTipoTcc pesquisaTipoTcc;
 	@OneToMany(mappedBy = "defesaAluno", fetch = FetchType.LAZY, targetEntity = DefesaComentario.class)
 	private List<DefesaComentario> comentarios = new ArrayList<DefesaComentario>();
-	private Lista<BancaProfessores> banca = new ArrayList<BancaProfessores>();
+	private List<BancaProfessor> banca = new ArrayList<BancaProfessor>();
 	private BigDecimal nota;
 	@Temporal(TemporalType.DATE)
 	@Column(name = "DATA")
@@ -63,6 +63,10 @@ public class DefesaAluno implements Serializable {
 		defesaComentario.adicionarComentario(defesaAluno, professorCurso,
 				comentario);
 		this.comentarios.add(defesaComentario);
+	}
+	
+	public void adicionarBancaProfessores(List<BancaProfessor> banca){
+		this.banca.addAll(banca);
 	}
 
 	public Long getIdDefesaAluno() {
@@ -111,12 +115,6 @@ public class DefesaAluno implements Serializable {
 
 	public void setTema(String tema) {
 		this.tema = tema;
-	}
-	
-	public void adicionarBancaProfessores(List<BancaProfessores> banca){
-		
-		banca.addAll(banca);
-		
 	}
 
 }
--- persistence/src/main/java/br/com/alvoradamaringa/persistence/DefesaAlunoDAOImpl.java
+++ persistence/src/main/java/br/com/alvoradamaringa/persistence/DefesaAlunoDAOImpl.java
@@ -10,7 +10,8 @@ import br.com.alvoradamaringa.domain.DefesaAluno;
 @Stateless
 public class DefesaAlunoDAOImpl extends GenericDAOImpl<Long, DefesaAluno> implements DefesaAlunoDAO {
 
-	public List<DefesaAluno> consultar(String nomeAluno, Date dataDefesa, String tema) {
+	@Override
+	public List<DefesaAluno> consultar(String nomeAluno, Date dataDefesa) {
 		return null;
 	}
 	
