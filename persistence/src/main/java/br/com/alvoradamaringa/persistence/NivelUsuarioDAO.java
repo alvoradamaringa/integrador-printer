50c69170e3a52754ddf177a573c38456c9a00d1b
Altieres de Matos <altitdb@gmail.com>
2015-07-01 18:59:51 -0300

    DAO para Nivel de Usuário

--- persistence/src/main/java/br/com/alvoradamaringa/persistence/NivelUsuarioDAO.java
+++ persistence/src/main/java/br/com/alvoradamaringa/persistence/NivelUsuarioDAO.java
@@ -1,10 +1,14 @@
 package br.com.alvoradamaringa.persistence;
 
+import java.util.List;
+
 import javax.ejb.Local;
 
 import br.com.alvoradamaringa.domain.NivelUsuario;
 
 @Local
 public interface NivelUsuarioDAO extends GenericDAO<Long, NivelUsuario> {
+
+	List<NivelUsuario> consultar(String descricao);
 
 }
