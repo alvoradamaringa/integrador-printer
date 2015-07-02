da2aac97b164fa9d6689728d69364e91ac5e2f16
Marcelo Beani da Costa Junior <marcelo666junior666@gmail.com>
2015-07-01 21:54:08 -0300

    Update DefesaServiceBean.java

--- business/src/main/java/br/com/alvoradamaringa/service/impl/DefesaServiceBean.java
+++ business/src/main/java/br/com/alvoradamaringa/service/impl/DefesaServiceBean.java
@@ -3,14 +3,11 @@ package br.com.alvoradamaringa.service.impl;
 import java.util.Date;
 import java.util.List;
 
-import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
-import br.com.alvoradamaringa.domain.Aluno;
 import br.com.alvoradamaringa.domain.BancaProfessor;
 import br.com.alvoradamaringa.domain.DefesaAluno;
 import br.com.alvoradamaringa.domain.DefesaComentario;
-import br.com.alvoradamaringa.persistence.DefesaAlunoDAO;
 import br.com.alvoradamaringa.service.exceptions.AlunoNaoInformadoException;
 import br.com.alvoradamaringa.service.exceptions.BancaException;
 import br.com.alvoradamaringa.service.exceptions.IntegridadeException;
@@ -19,52 +16,82 @@ import br.com.alvoradamaringa.service.spec.DefesaService;
 @Stateless
 public class DefesaServiceBean implements DefesaService {
 
-	@EJB
-	private DefesaAlunoDAO defesaAlunoDAO;
-	
 	@Override
 	public List<DefesaAluno> consultarDefesa(Date data, String tema,
-			String nomeAluno) {
+			String nomeAluno) throws AlunoNaoInformadoException {
+			
 		List<DefesaAluno> lista = defesaAlunoDAO.consultar(nomeAluno, data, tema);
 		return lista;
+	
+		
 	}
+
 	
 	@Override
 	public void salvarDefesa(List<BancaProfessor> bancaProfessores,
 			DefesaAluno defesaAluno) throws AlunoNaoInformadoException,
 			BancaException {
 		
-		Aluno aluno = defesaAluno.getAluno();
+			Aluno aluno = defesaAluno.getAluno();
+		
 
 		if (aluno == null) {
 			throw new AlunoNaoInformadoException();
 		}
 		
-		if (bancaProfessores.size() < 3){
-			throw new BancaException();
+		
+		if (bancaProfessor.size() < 3){
+			throw new BancaInvalidaException();
+			
 		}
 		
-		defesaAluno.adicionarBancaProfessores(bancaProfessores);
+		defesaAluno.adicionarBancaProfessor(bancaProfessores);
 		defesaAlunoDAO.salvar(defesaAluno);
+
+		
 	}
 		
-	@Override
-	public void excluirDefesa(DefesaAluno defesaAluno)
-			throws IntegridadeException {
-		if (defesaAluno == null){
-			throw new IntegridadeException();
-		}
-		defesaAlunoDAO.deletar(defesaAluno);
+	
+		
 	}
 
 	@Override
-	public void adicionarComentario(DefesaComentario DefesaComentario) {
+	public void excluirDefesa(DefesaAluno defesaAluno)
+			throws IntegridadeException {
+		
+		try{
+			defesaAlunoDAO.deletar(defesaAluno);
+		}catch(Exception ex){
+			throw new IntegridadeException();
+		}
+		
+				
+	}
+
+	@Override
+	public void adicionarComentario(DefesaComentario defesaComentario, 
+									DefesaAluno defesaAluno) {
+		
+		defesaAluno.getComentarios().add(defesaComentario);
+		defesaAlunoDAO.salvar(defesaAluno);
 		
 	}
+		
+
 
 	@Override
 	public void excluirComentario(DefesaComentario DefesaComentario) {
 		
+		throws IntegridadeException {
+			
+			try{
+				defesaComentarioDAO.deletar(defesaComentario);
+			}catch(Exception ex){
+				throw new IntegridadeException();
+			}
+			
+					
+		}
 	}
 
 }
