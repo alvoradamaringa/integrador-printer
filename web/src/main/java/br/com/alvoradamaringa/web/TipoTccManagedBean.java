a5197037a153930ad84c6c37f6a0c6b87d3ac9ef
Bruza <rodrigogrimmjow@gmail.com>
2015-07-09 20:41:30 -0300

    Alterado Telas de Cadastro e Consulta de Tipo TCC
    Alterado Telas de Cadastro e Consulta de Tipo TCC

--- web/src/main/java/br/com/alvoradamaringa/web/TipoTccManagedBean.java
+++ web/src/main/java/br/com/alvoradamaringa/web/TipoTccManagedBean.java
@@ -3,10 +3,14 @@ package br.com.alvoradamaringa.web;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import br.com.alvoradamaringa.domain.PesquisaTipoTcc;
+import br.com.alvoradamaringa.service.spec.PesquisaTipoTccService;
+import br.com.alvoradamaringa.web.util.FacesUtils;
 
 @ViewScoped
 @ManagedBean
@@ -14,6 +18,17 @@ public class TipoTccManagedBean implements Serializable {
 
 	private static final long serialVersionUID = 23L;
 	private PesquisaTipoTcc pesquisaTipoTcc;
+	private List<PesquisaTipoTcc> listaPesquisaTipoTcc;
+	private String descricao;
+	
+	@EJB
+	private PesquisaTipoTccService pesquisaTipoTccService;
+	
+	@PostConstruct
+	void init() {
+		descricao = null;
+		listaPesquisaTipoTcc = pesquisaTipoTccService.consultarPesquisaTipoTcc(null);
+	}
 	
 	public PesquisaTipoTcc getPesquisaTipoTcc() {
 		return pesquisaTipoTcc;
@@ -23,11 +38,45 @@ public class TipoTccManagedBean implements Serializable {
 		this.pesquisaTipoTcc = pesquisaTipoTcc;
 	}
 
-	public void CadastrarPesquisaTipoTcc(){
-		System.out.println("Cadastrando PesquisaTipoTcc");
+	public void CadastrarTipoTcc(){
+		pesquisaTipoTccService.salvarPesquisaTipoTcc(pesquisaTipoTcc);
+		FacesUtils.redirect("/tipotcc/ConsultaTipoTCC.xhtml");
 	}
 	
 	public List<PesquisaTipoTcc> getPesquisaTipoTccs(){
 		return null;
+	}
+
+	public PesquisaTipoTccService getPesquisaTipoTccService() {
+		return pesquisaTipoTccService;
+	}
+
+	public void setPesquisaTipoTccService(PesquisaTipoTccService pesquisaTipoTccService) {
+		this.pesquisaTipoTccService = pesquisaTipoTccService;
+	}
+	
+	public void consultarTipoTcc() {
+		setListaPesquisaTipoTcc(pesquisaTipoTccService.consultarPesquisaTipoTcc(descricao));
+	}
+	
+	public void menu() {
+		consultarTipoTcc();
+		FacesUtils.redirect("/tipotcc/ConsultaTipoTCC.xhtml");
+	}
+
+	public List<PesquisaTipoTcc> getListaPesquisaTipoTcc() {
+		return listaPesquisaTipoTcc;
+	}
+
+	public void setListaPesquisaTipoTcc(List<PesquisaTipoTcc> listaPesquisaTipoTcc) {
+		this.listaPesquisaTipoTcc = listaPesquisaTipoTcc;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
 }
